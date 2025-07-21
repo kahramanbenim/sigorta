7866460d1f4338ddbffc283855cf7bb67b4c5de8
kahramanbenim <coursemourse@gmail.com>
2025-07-22 00:57:33 +0300

    ekle5

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4186384001"
+    serviceWorkerVersion: "1997239967"
   }
 });
